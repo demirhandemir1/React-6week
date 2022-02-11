2644d1a8da69ca4984b9f33ece75778361f65847
Demirhan <demirhanndemir1@gmail.com>
2022-02-11 20:25:00 +0000

    graduation project

---
+++ week-6/src/companents/card/İndex.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react"
+import AddRecipes from "../AddRecipes"
+import Favorites from "../Favorites"
+import Search from "../Search"
+import CardButton from "./CardButton"
+import CardText from "./CardText"
+import "./_card.scss"
+import Zoom from "react-reveal/Fade"
+import axios from "axios"
+
+function Card({ recipes, setRecipes }) {
+  const [searchTerm, setSearchTerm] = useState("")
+  const [favorite, setFavorite] = useState([])
+
+  const addToFavorite = (id) => {
+    if (!favorite.includes(id)) setFavorite(favorite.concat(id))
+    console.log(id)
+  }
+
+   let filtered = !searchTerm
+    ? recipes
+    : recipes.filter((recipe) =>
+        recipe.title.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+
+  const deleteHandler = (id) => {
+    axios
+      .delete(`YOUR_URL_HERE/${id}`)
+      .then((res) => {
+        console.log(res)
+        const newRecipes = recipes.filter((item) => {
+          return item.id !== id
+        })
+        setRecipes(newRecipes)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }
+
+  return (
+    <div className="recipe__container">
+      <div className="recipe__extras">
+        <Zoom>
+          <Search setSearchTerm={setSearchTerm} />
+          <Favorites
+            recipes={recipes}
+            favorite={favorite}
+            setFavorite={setFavorite}
+          />
+          <AddRecipes recipes={recipes} setRecipes={setRecipes} />
+        </Zoom>
+      </div>
+      <div className="recipe__list">
+        {filtered.map((recipe) => {
+          return (
+            <div key={recipe.id} className="recipe__card">
+              {" "}
+              <Zoom>
+                <img
+                  src={recipe.image}
+                  alt="Recipe img comp"
+                  width={350}
+                  height={230}
+                />
+              </Zoom>
+              <CardText recipe={recipe} />
+              <CardButton
+                recipe={recipe}
+                addToFavorite={addToFavorite}
+                deleteHandler={deleteHandler}
+              />
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Card
